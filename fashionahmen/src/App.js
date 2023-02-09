d2c45c7478394a1db994c1ca49338873184fbd15
fablorv <fablorvdz@gmail.com>
2023-02-09 19:59:05 +0100

    setting up the html

---
+++ fashionahmen/src/App.js
@@ -0,0 +1,48 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <button>arrow</button>
+        <p>logo here</p>
+      </div>
+      <div className="middle">
+        <div className="item">
+          <button>left button</button>
+          <p>img of item</p>
+          <button>right button</button>
+        </div>
+        <div className="itemswap">
+          <button>dots </button>
+        </div>
+      </div>
+      <div className="nameprice">
+        <div className="name">
+          <p>BLACK HOODIE</p>
+          <p> HANMA store </p>
+        </div>
+        <div className="price">
+          <p>3600 DZD </p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="colors">  
+          <p> color </p>
+          <button> white </button>
+          <button> BLACK </button>
+        </div>
+        <div className="buyheart">
+          <button>BUY NOW </button>
+          <p> heart pic </p>
+        </div>
+      </div>
+      
+        
+      ahmed fashion show
+    </div>
+  );
+}
+
+export default App;
