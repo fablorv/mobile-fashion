216ac8cd49be117f420eee51c4681dfd0063905b
fablorv <fablorvdz@gmail.com>
2023-02-21 07:34:34 +0100

    fixed positioning

--- fashionahmen/src/App.js
+++ fashionahmen/src/App.js
@@ -8,6 +8,8 @@ import circle from './imgs/circle.svg'
 
 
 function App() {
+
+
   return (
     <div className="App">
       <div className="header">
@@ -21,7 +23,9 @@ function App() {
           <button><img src={leftb} alt="leftbtn" className="rightb"/></button>
         </div>
         <div className="itemswap">
-          <button><img src={circle} alt="circle"/> </button>
+          <button ><img className="swapimg" src={circle} alt="circle"/> </button>
+          <button><img  className="swapimg" src={circle} alt="circle"/> </button>
+          <button><img className="swapimg" src={circle} alt="circle"/> </button>
         </div>
       </div>
       <div className="nameprice">
